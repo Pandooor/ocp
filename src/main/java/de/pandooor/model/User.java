bfb51b0a09a5d98ed01ca73c85bfaf71758c33bc
pandooor <liebig-jan@gmx.net>
2021-03-21 11:00:11 +0100

    basic user states

--- src/main/java/de/pandooor/model/User.java
+++ src/main/java/de/pandooor/model/User.java
@@ -4,4 +4,39 @@ import java.io.Serializable;
 
 public class User extends AbstractEntity implements Serializable {
 
+    //add serialVersionUID
+
+    private String firstname;
+    private String lastname;
+
+    public User() {
+
+    }
+
+    public User(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public User(int id, String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        setId(id);
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 }
